00ed9f738d9795fe7eb60abd626d324907c44190
shashidharyellenki <91315935+shashidharyellenki@users.noreply.github.com>
2021-09-25 11:53:05 +0530

    Implemented addship

--- battleship.py
+++ battleship.py
@@ -89,7 +89,15 @@ Parameters: 2D list of ints ; int
 Returns: 2D list of ints
 '''
 def addShips(grid, numShips):
-    return
+     count=0
+    while count < numShips:
+        ship2= createShip()
+        if checkShip(grid, ship2) == True:
+            for i in range(len(ship2)):
+                grid[ship2[i][0]][ship2[i][1]] =SHIP_UNCLICKED
+            count+=1
+    return grid
+    
 
 
 '''
